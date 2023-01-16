1f8a79f05e2c193cf1d3cdb425c1dd9dc7e98a46
Justin Heyes-Jones <justinhj@gmail.com>
2023-01-15 22:22:51 -0800

    chap 2

---
+++ .spago/run.js
@@ -0,0 +1,3 @@
+import { main } from 'file:///Users/justin.heyes-jones/projects/purescript-book/output/Test.Main/index.js'
+
+main()
--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,14 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Number (sqrt,pi)
+import Data.Int (rem)
+
+diagonal :: Number -> Number -> Number
+diagonal b h = sqrt ((b * b) + (h * h))
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r 
+
+leftoverCents :: Int -> Int
+leftoverCents a = rem a 100
