f0f75296ea8c9315fa9039aba7a63ba6004386a5
Justin Heyes-Jones <justinhj@gmail.com>
2023-02-22 20:35:55 -0800

    tidy up state example

--- exercises/chapter11/test/MySolutions.purs
+++ exercises/chapter11/test/MySolutions.purs
@@ -8,16 +8,14 @@ import Data.String.CodeUnits (toCharArray)
 
 testParens :: String -> Boolean
 testParens arr = execState (countB $ toCharArray arr) 0 == 0
-
-countB :: Array Char -> State Int Unit
-countB = traverse_ (\next -> modify \c -> updateC c next)
   where 
-    updateC count next = 
-      if count < 0 then
-        count
-      else if next == '(' then
-        count + 1
-      else if next == ')' then
-        count - 1
-      else
-        count
+    countB :: Array Char -> State Int Unit
+    countB = traverse_ (\next -> modify \c -> updateC c next)
+      where 
+        updateC count next = 
+          if next == '(' && count >= 0 then
+            count + 1
+          else if next == ')' && count >= 0 then
+            count - 1
+          else
+            count
